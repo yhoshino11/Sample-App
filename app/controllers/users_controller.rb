842776fbdcfc7e181d2cd2df9f919d27bf32d68c
Yu Hoshino <yhoshino11@gmail.com>
2014-01-24 11:50:51 +0900

    9.3.3ページネーション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
   
   def show
     @user = User.find(params[:id])
@@ -25,7 +30,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
       sign_in @user
@@ -37,6 +41,14 @@ class UsersController < ApplicationController
 
   private
   def signed_in_user
-    redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -31,6 +31,7 @@ describe "AuthenticationPages" do
         before { valid_signin(user) }
 
         it { should have_selector('title', text: user.name) }
+        it { should have_link('Users', href: users_path) }
         it { should have_link('Profile', href: user_path(user)) }
         it { should have_link('Settings', href: edit_user_path(user)) }
         it { should have_link('Sign out', href: signout_path) }
@@ -60,6 +61,42 @@ describe "AuthenticationPages" do
           before { put user_path(user) }
           specify { response.should redirect_to(signin_path) }
         end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          # it { should have_selector('title', text: 'Sign in') }
+        end
+      end
+
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          fill_in "Email", with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
+
+        describe "after signing in" do
+          it "should render the desired protected page" do
+            page.should have_selector('title', text: 'Edit user')
+          end
+        end
+      end
+    end
+
+    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+      before { sign_in user }
+
+      describe "visiting Users#edit page" do
+        before { visit edit_user_path(wrong_user) }
+        it { should_not have_selector('title', text: full_title('Edit user')) }
+      end
+
+      describe "submitting a PUT request to the Users#update action" do
+        before { put user_path(wrong_user) }
+        specify { response.should redirect_to(root_path) }
       end
     end
   end
